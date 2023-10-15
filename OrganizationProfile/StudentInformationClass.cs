6b6a6f6fb48adf0c2e44607a256b1fa57a7e0d20
waynedominicc <mercadillo.260317@balagtas.sti.edu.ph>
2023-10-16 01:06:36 +0800

    jj

---
+++ OrganizationProfile/StudentInformationClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizationProfile
+{
+    internal class StudentInformationClass
+    {
+        public static int SetStudentNo = 0;
+        public static int SetContactNo = 0;
+        public static int SetAge = 0;
+        public static string SetProgram = " ";
+        public static string SetGender = " ";
+        public static string SetBirthday = " ";
+        public static string SetFullName = " ";
+
+    }
+}
